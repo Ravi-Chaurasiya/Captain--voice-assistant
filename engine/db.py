95df38b86c04c3e9f5935d9373996bd2aa1d0ff2
Ravi Chaurasiya <ravichaurasiya693@gmail.com>
2025-05-21 09:51:46 +0530

    database updated

--- engine/db.py
+++ engine/db.py
@@ -7,12 +7,13 @@ cursor = con.cursor()
 query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
 cursor.execute(query)
 
-#query = "INSERT INTO sys_command VALUES (null,'Note Pad', 'C:\\Program Files\\Microsoft Office\\root\\Office16\\ONENOTE.exe')"
+#query = "INSERT INTO sys_command VALUES (null,'C drive', 'C:\\')"
+
 #cursor.execute(query)
 #con.commit()
 
-query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
-cursor.execute(query)
+#query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+#cursor.execute(query)
 
 #query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
 #cursor.execute(query)
@@ -36,8 +37,8 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, n
 # # Read data from CSV and insert into SQLite table for the desired columns
 #with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
     #csvreader = csv.reader(csvfile)
-     #for row in csvreader:
-        # selected_data = [row[i] for i in desired_columns_indices]
+    #for row in csvreader:
+         #selected_data = [row[i] for i in desired_columns_indices]
          #cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
 
 # # Commit changes and close connection
